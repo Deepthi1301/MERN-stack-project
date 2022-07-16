1b8da512c151ec5e215045d2c9de77f80effb651
Deepthi <55611188+Deepthi1301@users.noreply.github.com>
2022-07-16 17:42:18 +0530

    Add files via upload

---
+++ backend/src/db-config.js
@@ -0,0 +1,5 @@
+// TODO: Rename the filename from TODO-db-config.js to db-config.js
+// Paste your MongoDB API key here
+module.exports = {
+	database: "mongodb+srv://deepthi1301:quizproject@cluster0.yzk5q.mongodb.net/?retryWrites=true&w=majority"
+}
