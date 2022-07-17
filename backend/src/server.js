d0dfabbcf1765c49216e58699e95098d80f090b2
Deepthi1301 <deepthi.peter2001@gmail.com>
2022-07-17 16:09:07 +0530

    heroku deployable

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,7 @@
 'use strict'
+
+const dotenv = require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const path = require('path')
@@ -7,17 +10,25 @@ const quizzesRoute = require('./Routes/Quizzes')
 
 // Hosting Frontend
 // Create a production build of the frontend and paste the files in the public folder
-app.use(express.static(path.join(__dirname, '/public/')))
+// app.use(express.static(path.join(__dirname, '/public/')))
 
 // Middleware
 app.use(express.json())
 app.use('/API/users', userRoute)
 app.use('/API/quizzes', quizzesRoute)
 
-app.use('*', (req, res) => {
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'../../frontend/build')))
+
+    app.get('*',(req,res) => res.sendFile(path.resolve(__dirname,'../../','frontend','build','index.html')))
+} else {
+    app.get('/',(req,res) => res.send("Please set to production"))
+}
+/* app.use('*', (req, res) => {
 	res.sendFile(path.join(__dirname, '../../frontend/public/index.html'))
-})
+}) */
 // Listening to APIs
 app.listen(process.env.PORT || 8000, () =>
-	console.log('Listening on Port 8000')
+	console.log('Listening on ' + process.env.PORT)
 )
+
